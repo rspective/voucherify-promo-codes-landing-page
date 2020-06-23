5083f9471ee310877de8c3f5d027de7f7c39c2ce
Piotr Gacek <pgacek0@gmail.com>
2020-06-23 11:09:44 +0200

    Copied demo landing page

---
+++ script.js
@@ -0,0 +1,32 @@
+/* If you're feeling fancy you can add interactivity 
+    to your site with Javascript */
+
+// prints "hi" in the browser's dev tools console
+
+
+
+
+/*console.log('hi');
+
+const Voucherify = window.Voucherify;
+
+Voucherify.initialize(
+  "9cf4abeb-f637-4360-b375-891c8173929f",
+  "54824629-59d5-49e9-98b8-94c9658e06f5"
+);
+
+
+const customer = {
+  source_id: "test@wp.pl"
+};
+
+Voucherify.publish("voucherify-redemption-example", { customer }, (response) => { 
+  console.log(response);
+  const codeV = response.voucher.code;
+  console.log(codeV)
+  document.querySelector(".code-field").innerHTML = codeV;
+});
+
+
+*/
+
